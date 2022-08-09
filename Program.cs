6677f071439e6be875e96470249601b4a58e7335
tomasoyarzabal <tomasoyarzabal135@gmail.com>
2022-08-09 09:40:23 -0300

    casa

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace casa_computacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tf, sueldo;
+            Console.WriteLine("ingrese el total facturado");
+            tf = int.Parse(Console.ReadLine());
+            tf = tf * 5 / 100;
+            sueldo = 15000;
+            sueldo = tf + sueldo;
+            Console.WriteLine("su salario a cobrar es de " + sueldo + "$");
+            
+                
+            
+
+        }
+    }
+}
